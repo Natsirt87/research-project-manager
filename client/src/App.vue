b87585dc54950c9e671e089b8b8ab7a14d1e5c72
Natsirt87 <tristancommons@outlook.com>
2023-02-22 22:15:45 -0800

    Added dark mode toggle support & basic setup.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,26 @@
 <script setup>
 import { RouterView } from "vue-router";
-import { NConfigProvider } from "naive-ui";
+import { NConfigProvider, NButton } from "naive-ui";
 import Navbar from './components/Navbar.vue';
+import { darkTheme, lightTheme } from "naive-ui";
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+
+const themeOverrides = {
+  common: {
+    
+  }
+}
 </script>
 
 <template>
-  <n-config-provider>
+  <n-config-provider :theme="isDark ? darkTheme : lightTheme" :theme-overrides="themeOverrides">
     <Navbar/>
+    <n-button @click="toggleDark()">Toggle Dark Mode</n-button>
+    <h1 class="bg-primary">{{ isDark }}</h1>
+    
     <RouterView />
   </n-config-provider>
 </template>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode:'jit',
+  darkMode: 'class',
   content: [
     "./src/**/*.{html, js, ts, vue}", 
     "./src/**/*"
   ],
   theme: {
-    extend: {},
+    colors: {
+      "primary": "#63E2B7FF",
+      "info": "#70C0E8FF"
+    },
   },
   plugins: [],
 }
