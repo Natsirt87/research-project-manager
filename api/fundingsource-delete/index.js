930e3833f66779a0c47547afc37d556498a32001
Brandon <41244657+btdeguia@users.noreply.github.com>
2023-03-09 10:17:45 -0800

    Creates Azure functions for queries for FundingSource table

---
+++ api/fundingsource-delete/index.js
@@ -0,0 +1,47 @@
+const sql = require('mssql');
+
+module.exports = async function (context, req) {
+    context.log("fundingsource-delete processed a request");
+
+    try {
+      const database = await sql.connect(process.env.SQLConnectionString);
+
+      const id = req.body;
+
+      const query =
+      `
+        DELETE FROM FundingSource
+        WHERE FundingSource.ID = ${id}
+      `;
+
+      const result = await database.request().query(query);
+
+      database.close();
+
+      context.res.json({
+        status: 200,
+        headers: {"Content-Type": "application/json"},
+        body: result.recordset
+      })
+
+    } catch (error) {
+      context.res.json({
+        status: 500,
+        body: "Failed to connect to database with error: " + error
+      });
+    }
+}
+
+// module.exports = async function (context, req) {
+//     context.log('JavaScript HTTP trigger function processed a request.');
+
+//     const name = (req.query.name || (req.body && req.body.name));
+//     const responseMessage = name
+//         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+//         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+//     context.res = {
+//         // status: 200, /* Defaults to 200 */
+//         body: responseMessage
+//     };
+// }
